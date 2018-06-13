3d0836f9dbde5e18f36ef12b1709f741f8171115
teamm8 <leonjaeggi@gmail.com>
2018-06-14 00:26:34 +0100

    add allowed hosts for LAN testing

--- bigday/settings.py
+++ bigday/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.25", "localhost"]
 
 
 # Application definition
